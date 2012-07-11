6fc16f12379f07b50b672490102b6ded2cc3e979
Piotr S. Staszewski <p.staszewski@gmail.com>
2012-07-11 12:20:22 +0200

    Rewritten structure.

--- getlinks.go
+++ getlinks.go
@@ -13,15 +13,16 @@ import (
   "net/http"
   "os"
   "regexp"
-  "strings"
 )
 
 var (
   verbose bool
-  follow_arg string
-  follow_type string
-  follow_types = []string{"text", "url", "class", "id"}
-  tasks map[string]*regexp.Regexp
+  follow bool
+  follow_rxp *regexp.Regexp
+  follow_limit int
+  par_limit int
+  url string
+  rxps []*regexp.Regexp
 )
 
 
@@ -30,26 +31,46 @@ func die(msg string, code int) {
   os.Exit(code)
 }
 
+func make_regexp(arg string) *regexp.Regexp {
+  rxp, err := regexp.Compile(arg)
+  if err != nil {
+    die("Error compiling regexp '" + arg + "'!", 2)
+  }
+  if rxp.NumSubexp() != 1 {
+    die("Regexp '" + arg + "' has to have exactly one group!", 2)
+  }
+  return rxp
+}
+
 func setup() {
-  flag.BoolVar(&verbose, "v", false, "Be verbose")
-  flag.StringVar(&follow_arg, "f", "", "Follow link regexp")
-  flag.StringVar(&follow_type, "t", "text", "Follow link by " + strings.Join(follow_types, "|"))
+  flag.Usage = func() {
+    fmt.Fprintf(os.Stderr, "Usage: %s [options] url regexp regexp...\n", os.Args[0])
+    flag.PrintDefaults()
+  }
+
+  var follow_str string
+  flag.BoolVar(&verbose, "v", false, "Be verbose on stderr")
+  flag.StringVar(&follow_str, "f", "", "Follow link regexp")
+  flag.IntVar(&follow_limit, "l", 0, "Limit following to n times (0 = no limit)")
+  flag.IntVar(&par_limit, "p", 5, "Limit parallel processing to n threads (0 = no limit)")
   flag.Parse()
-  args := flag.Args()
-  if len(args) == 0 || len(args) % 2 != 0 {
-    die("You have to specify at least one url regexp pair!", 5)
+
+  if follow_str != "" {
+    follow_rxp = make_regexp(follow_str)
+    follow = true
   } else {
-    n := len(args)
-    tasks = make(map[string]*regexp.Regexp, n / 2)
-    for i := 0; i < n; i += 2 {
-      arg := args[i+1]
-      rxp, err := regexp.Compile(arg)
-      if err != nil {
-        die("Error parsing regexp '" + arg + "'!", 2)
-      } else {
-        tasks[args[i]] = rxp
-      }
-    }
+    follow = false
+  }
+
+  args := flag.Args()
+  n := len(args)
+  if n < 2 {
+    die("You have to specify at least url and one regexp!", 5)
+  }
+  url = args[0];
+  rxps = make([]*regexp.Regexp, n - 1)
+  for i := 1; i < n; i++ {
+    rxps[i - 1] = make_regexp(args[i])
   }
 }
 
@@ -75,19 +96,17 @@ func fetch(url string) (string, error) {
 func main() {
   setup()
   say("Hello there")
-  if follow_arg != "" {
-    say("Following type: " + follow_type)
-    say("Following regexp: " + follow_arg)
+  if follow {
+    say("Following regexp: " + follow_rxp.String())
   }
-  for url, rxp := range tasks {
-    say("Processing link '" + url + "' with regexp '" + rxp.String() + "'")
-    body, err := fetch(url)
-    if err != nil {
-      log.Fatalln("Error fetching url '" + url + "'!")
-    }
-    //fmt.Println(body)
+  body, err := fetch(url)
+  if err != nil {
+    die("Error loading the primary url!", 5)
+  }
+  for _, rxp := range rxps {
+    say("Processing regexp '" + rxp.String() + "'")
     matches := rxp.FindAllStringSubmatch(body, -1)
-    fmt.Printf("Found %d\n", len(matches))
+    say(fmt.Sprintf("Found %d matches", len(matches)))
     for i := 0; i < len(matches); i++ {
       fmt.Println(matches[i][1])
     }
