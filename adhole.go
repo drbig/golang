850fbfe7c36c6dd7a60020b3083659b7cb0887be
Piotr S. Staszewski <p.staszewski@gmail.com>
2014-06-21 20:57:01 +0200

    Ditch TCP, parse header

--- adhole.go
+++ adhole.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"bytes"
+	"encoding/binary"
 	"encoding/hex"
+	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -11,75 +13,78 @@ import (
 	"os/signal"
 )
 
-func runServerTCP(proto string, addr string, port int) {
-	fulladdr := fmt.Sprintf("%s:%d", addr, port)
-
-	srv, err := net.Listen(proto, fulladdr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer srv.Close()
-
-	log.Printf("Started %s server at %s\n", proto, fulladdr)
-	for {
-		conn, err := srv.Accept()
-		if err != nil {
-			log.Println("ERROR:", proto, err)
-			continue
-		}
-
-		log.Println("New connection", proto, conn)
-		go handleTCP(conn)
-	}
-
-	panic("not reachable")
+type Header struct {
+	Id      uint16
+	Flags   uint16
+	Qtcount uint16
+	Ancount uint16
+	Arcount uint16
+	Adcount uint16
 }
 
-func runServerUDP(proto string, addr string, port int) {
+type Question struct {
+	Query  string
+	Qtype  uint16
+	Qclass uint16
+}
+
+func (h *Header) String() string {
+	return fmt.Sprintf("Id: %X Flags: %b Counts: %d/%d/%d/%d",
+		h.Id, h.Flags, h.Qtcount, h.Ancount, h.Arcount, h.Adcount)
+}
+
+func parseHeader(raw []byte) (hdr *Header, err error) {
+	if len(raw) < 12 {
+		err = errors.New(fmt.Sprintf("parseHeader: not enough raw data"))
+		return
+	}
+
+	hdr = &Header{}
+	reader := bytes.NewReader(raw[:12])
+	err = binary.Read(reader, binary.BigEndian, hdr)
+
+	return
+}
+
+func runServerDNS(addr string, port int) {
 	fulladdr := fmt.Sprintf("%s:%d", addr, port)
 	udpaddr := &net.UDPAddr{
 		IP:   net.ParseIP(addr),
 		Port: port,
 	}
 
-	srv, err := net.ListenUDP(proto, udpaddr)
+	srv, err := net.ListenUDP("udp", udpaddr)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer srv.Close()
 
-	log.Printf("Started %s server at %s\n", proto, fulladdr)
+	log.Println("Started DNS server at", fulladdr)
 	buf := make([]byte, 64)
 	oobuf := make([]byte, 64)
 	for {
 		_, _, _, addr, err := srv.ReadMsgUDP(buf, oobuf)
 		if err != nil {
-			log.Println("ERROR:", proto, err)
+			log.Println("DNS ERROR:", err)
 			continue
 		}
 
-		log.Println("New message", proto, addr)
-		go handleUDP(buf, addr)
+		log.Println("DNS: New message from", addr)
+		go handleDNS(buf, addr)
 	}
 
 	panic("not reachable")
 }
 
-func handleUDP(payload []byte, addr *net.UDPAddr) {
-	dump(payload)
-}
-
-func handleTCP(c net.Conn) {
-	buf := make([]byte, 64)
-	_, err := io.ReadAtLeast(c, buf, 12)
+func handleDNS(payload []byte, addr *net.UDPAddr) {
+	header, err := parseHeader(payload)
 	if err != nil {
-		log.Println("ERROR:", err)
-		return
+		log.Println("DNS ERROR:", err)
+	} else {
+		fmt.Println("Header:", header)
 	}
 
-	dump(buf)
-	log.Println("Connection closed tcp", c)
-	c.Close()
+	dump(payload)
 }
 
 func dump(data []byte) {
@@ -89,9 +94,7 @@ func dump(data []byte) {
 }
 
 func main() {
-
-	go runServerTCP("tcp", "192.168.0.11", 5353)
-	go runServerUDP("udp", "192.168.0.11", 5354)
+	go runServerDNS("192.168.0.11", 5354)
 
 	sig := make(chan os.Signal)
 	signal.Notify(sig, os.Interrupt)
