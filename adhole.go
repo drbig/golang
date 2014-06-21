2df1c87a949c2b0d67fda7e87631b2d636cb5ddc
Piotr S. Staszewski <p.staszewski@gmail.com>
2014-06-21 21:52:21 +0200

    Parse Question

--- adhole.go
+++ adhole.go
@@ -23,14 +23,20 @@ type Header struct {
 }
 
 type Question struct {
-	Query  string
-	Qtype  uint16
-	Qclass uint16
+	Name  string
+	Flags struct {
+		Type  uint16
+		Class uint16
+	}
 }
 
 func (h *Header) String() string {
 	return fmt.Sprintf("Id: %X Flags: %b Counts: %d/%d/%d/%d",
 		h.Id, h.Flags, h.Qtcount, h.Ancount, h.Arcount, h.Adcount)
+}
+
+func (q *Question) String() string {
+	return fmt.Sprintf("Name: %s Type: %d Class: %d", q.Name, q.Flags.Type, q.Flags.Class)
 }
 
 func parseHeader(raw []byte) (hdr *Header, err error) {
@@ -42,6 +48,34 @@ func parseHeader(raw []byte) (hdr *Header, err error) {
 	hdr = &Header{}
 	reader := bytes.NewReader(raw[:12])
 	err = binary.Read(reader, binary.BigEndian, hdr)
+
+	return
+}
+
+func parseQuestion(raw []byte) (qst *Question, end int, err error) {
+	var idx int
+	var buf bytes.Buffer
+
+loop:
+	for i, val := range raw {
+		switch val {
+		case 0:
+			idx = i + 1
+			break loop
+		case 2: // '.', why so?
+			buf.WriteByte(46)
+		default:
+			buf.WriteByte(val)
+		}
+	}
+
+	qst = &Question{
+		Name: buf.String(),
+	}
+
+	reader := bytes.NewReader(raw[idx:])
+	err = binary.Read(reader, binary.BigEndian, &qst.Flags)
+	end = idx + 4
 
 	return
 }
@@ -82,6 +116,17 @@ func handleDNS(payload []byte, addr *net.UDPAddr) {
 		log.Println("DNS ERROR:", err)
 	} else {
 		fmt.Println("Header:", header)
+		offset := 13
+		for i := uint16(0); i < header.Qtcount; i++ {
+			question, end, err := parseQuestion(payload[offset:])
+			if err != nil {
+				log.Println("DNS ERROR:", err)
+				break
+			} else {
+				fmt.Println("Question:", question)
+				offset = offset + end
+			}
+		}
 	}
 
 	dump(payload)
